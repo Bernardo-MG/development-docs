7a99c5cac355556e4e7dc3085400226e68a62fde
Bernardo Martínez Garrido <programming@wandrell.com>
2016-03-19 14:16:28 +0100

    Added setup class for tox.

---
+++ setup.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import ast
+import re
+import sys
+import io
+from os.path import dirname
+from os.path import join
+from codecs import open
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as test_command
+
+"""
+PyPI configuration module.
+This is prepared for easing the generation of deployment files.
+"""
+
+__license__ = 'MIT'
+
+# Regular expression for the version
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+# Test requirements
+_tests_require = ['tox']
+
+
+# Gets the long description from the readme
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+# Gets the version for the source folder __init__.py file
+with open('cwr/__init__.py', 'rb', encoding='utf-8') as f:
+    version_lib = f.read()
+    version_lib = _version_re.search(version_lib).group(1)
+    version_lib = str(ast.literal_eval(version_lib.rstrip()))
+
+
+class _ToxTester(test_command):
+    def finalize_options(self):
+        test_command.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import tox
+
+        errcode = tox.cmdline(self.test_args)
+        sys.exit(errcode)
+
+
+setup(
+    name='Wandrell Development Docs',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    version=version_lib,
+    description='Development docs for Wandrell\'s projects.',
+    author=u'Bernardo Martínez Garrido',
+    author_email='programming@wandrell.com',
+    license='MIT',
+    url='https://github.com/Bernardo-MG/development-docs',
+    download_url='https://github.com/Bernardo-MG/development-docs',
+    keywords=['Development manual', 'Wadnrell'],
+    platforms='any',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+    long_description=read('README.md'),
+    install_requires=[
+    ],
+    tests_require=_tests_require,
+    extras_require={'test': _tests_require},
+    cmdclass={'test': _ToxTester},
+)
